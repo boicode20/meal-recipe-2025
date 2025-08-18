59fcba729f0d289295aa053420c3ea5bbd747b8c
Joey Tamondong <joeytamondong2001@gmail.com>
2025-08-18 19:19:40 +0800

    Login Design Done

--- Frontend/src/Auth/AuthForm/LoginForm.jsx
+++ Frontend/src/Auth/AuthForm/LoginForm.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import { FaRegUser } from "react-icons/fa";
 import { FaRegEyeSlash } from "react-icons/fa6";
 import { FaRegEye } from "react-icons/fa";
 
 const LoginForm = () => {
+  const [show,setShow] = useState(false)
   return (
     <form className='w-full h-auto flex flex-col gap-2' >
         <div className="input-data flex flex-col ">
@@ -17,13 +18,21 @@ const LoginForm = () => {
         <div className="input-data flex flex-col ">
           <label htmlFor="password">Password</label>
           <div className="input-parent w-full relative flex items-center">
-            <input className='border outline-0 w-full h-[36px] rounded-[3px] pl-2 pr-10' type="password" name='password' id='password' />
-            <FaRegEyeSlash className='absolute right-3 cursor-pointer'/>
+            <input className='border outline-0 w-full h-[36px] rounded-[3px] pl-2 pr-10' type={show?"text":"password"} name='password' id='password' />
+            {
+              show?<FaRegEye className='absolute right-3 cursor-pointer' onClick={()=>setShow(!show)}/>:<FaRegEyeSlash className='absolute right-3 cursor-pointer' onClick={()=>setShow(!show)} />
+            }
 
           </div>
         </div>
         <Link className='text-[.9rem] underline'><p>Forgot password?</p></Link>
         <button className='login-btn w-full h-[37px] bg-[var(--login-btn)] rounded-[3px] text-[#fff] font-semibold cursor-pointer' type='submit'>Login</button>
+        <div className="or flex w-full h-auto items-center  gap-1">
+          <span className='w-full h-[1px] bg-[#969696]'></span>
+          <p className='text-[.8rem] mt-[-3px]'>or</p>
+          <span className='w-full h-[1px] bg-[#969696]'></span>
+        </div>
+        <button className='register-btn w-full h-[37px] bg-[var(--register-btn)] rounded-[3px] text-[#fff] font-semibold cursor-pointer' type='submit'>Register</button>
     </form>
   )
 }
